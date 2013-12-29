484cc35728c88e838e98d0888e1bd50d5fc1effa
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2013-12-29 16:19:05 +0200

    1.

---
+++ init.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+export PATH=$PATH:~/arm/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin
---
+++ src/boot/boot.s
@@ -0,0 +1,4 @@
+.extern kernel_main
+.globl _start
+_start:
+	bl kernel_main                                                                                                                                                                                                	                                            
